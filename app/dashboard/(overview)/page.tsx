35d7ab144165d44b7fc5d24811d55076c6920b5d
chr-d <275697199+chr-d@users.noreply.github.com>
2026-09-03 10:47:23 +0200

    make dashboard overview a dynamic page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,4 +1,4 @@
-import CardWrapper, { Card } from "@/app/ui/dashboard/cards";
+import CardWrapper from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
@@ -9,6 +9,7 @@ import {
   RevenueChartSkeleton,
 } from "@/app/ui/skeletons";
 
+export const dynamic = "force-dynamic";
 export default async function Page() {
   return (
     <main>
